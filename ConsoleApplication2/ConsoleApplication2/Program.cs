5e483474063498dc9b5ef88132640d9ab2565042
famturiop <vorobyev.seva.rules@gmail.com>
2014-07-08 20:24:13 +0400

    656

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,27 +1,29 @@
 ﻿namespace обучалка
 {
-    class cl            
+    class airport            
     {
         private 
         int maxspeed = 0;
         int speed = 0;
         int fuel = 0;
         int fuelpersec = 0;
-        int maxfuel = 700;
+        int maxfuel = 10;
         int height = 0;
         int maxheight = 900;
         
-        public cl(int startfuel)        
+        public airport(int startfuel)        
         {
             fuel = startfuel;              
-            Console.WriteLine("Диспетчерская.");
+            Console.WriteLine("Диспетчерская. Система управления самолетами.");
         }
-        public void getfuel()       
+        public void getfuel()  // заправка     
         {
             fuel = maxfuel;
-        public void morespeed(int a)
+        }
+        public void morespeed(int a)   // увелич. скорости
         {
-            if speed + a < maxspeed 
+            fuel = fuel - a*maxfuel*5/100;  //на каждое изм. скорости требуется 5 проц от макс кол-ва топлива
+            if (speed + a < maxspeed) 
             {
                 speed = speed + a;
             }
@@ -30,9 +32,10 @@
                 Console.WriteLine("Нельзя увеличить скорость на данное значение, превышает макс. скорость.");
             }
         }
-        public void lessspeed(int a)
+        public void lessspeed(int a)   // уменьш. скорости
         {
-            if speed - a > 0 
+            fuel = fuel - a*maxfuel*5/100;  
+            if (speed - a > 0) 
             {
                 speed = speed - a;
             }
@@ -40,15 +43,16 @@
             {
                 speed = 0;
             }
-            if speed < maxspeed*0,1
+            if (speed < maxspeed / 100)   // при слишком низкой скорости самолет разбивается
             {
                 height = 0;
-                Console.WriteLine("Слишком низкая скорость. Самолет упал и разбился =\.");
+                Console.WriteLine("Слишком низкая скорость. Самолет упал и разбился.");
             }
         }
-        public void moreheight(int a)
+        public void moreheight(int a)   // увелич. высоты
         {
-            if height + a < maxspeed 
+            fuel = fuel - a*maxfuel/100;  //на каждое изм. высоты требуется 10 проц от макс кол-ва топлива
+            if (height + a < maxspeed) 
             {
                 height = height + a;
             }
@@ -57,15 +61,16 @@
                 Console.WriteLine("Нельзя увеличить высоту на данное значение, превышает макс. высоту.");
             }
         }
-        public void lessheight(int a)
+        public void lessheight(int a)   // уменьш. высоты
         {
-            if height - a > 0 
+            fuel = fuel - a*maxfuel/100;
+            if (height - a > 0) 
             {
                 height = height + a;
             }
-            else
+            else   // при уменьш. высоты до уровня земли проверяется скорость самолета, если она слишком большая самолет разбивается
             {
-                if speed > maxspeed*0,5
+                if (speed > maxspeed*50/100)
                 {
                     height = 0;
                     Console.WriteLine("Самолет разбился.");
