89a6abb457008520a5efa0fb673ed67a608d8557
famturiop <vorobyev.seva.rules@gmail.com>
2014-07-10 02:12:21 +0400

    strs

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
+
 namespace обучалка
 {
     class airport
     {
-        private
+        public
         int maxspeed = 0;
         int speed = 0;
         int fuel = 0;
@@ -19,14 +21,16 @@ namespace обучалка
         int maxheight = 900;
         bool engine = false;
 
+
         int ind; //это индекс объекта. это временная переменная, которую можно удалить, если реализуешь иначе. но она пока для того, что бы вызов метода "респаун" не ругался.***
+        //но сейчас он нафиг не нужен, так как мы создаём только 1 экземпляр
 
 
         public airport()
         {
             fuel = 10;
             Console.WriteLine("Диспетчерская. Система управления самолетами.");
-            
+
         }
         public void getfuel()  // заправка     
         {
@@ -34,7 +38,7 @@ namespace обучалка
         }
         public void morespeed(int a)   // увелич. скорости
         {
-            
+
             fuel = fuel - a * maxfuel * 5 / 100;  //на каждое изм. скорости требуется 5 проц от макс кол-ва топлива
             if (speed + a < maxspeed)
             {
@@ -44,7 +48,7 @@ namespace обучалка
             {
                 Console.WriteLine("Нельзя увеличить скорость на данное значение, превышает макс. скорость.");
             }
-            
+
         }
         public void lessspeed(int a)   // уменьш. скорости
         {
@@ -88,7 +92,7 @@ namespace обучалка
                 {
                     height = 0;
                     Console.WriteLine("Самолет разбился.");
-                    
+
                 }
                 else
                 {
@@ -101,74 +105,85 @@ namespace обучалка
         {
             engine = true;
             Console.WriteLine("Запуск двигателя.");
+            met.center();
         }
         public void stopengine()
         {
             engine = false;
             Console.WriteLine("Отключение двигателя.");
-            Program.center();
+            met.center();
+            
         }
     }
-    
+
+
+    static class met    //класс-набор методов
+    {
+        public static airport plane = new airport();//наш любимый самолётик
+        static void print_menu()//просто пусть печать юудет в отделном вызываемом методе. в методе центр эта писанина лишняя наверное. да и так удобнее, хотя возможно это просто я так привык делать. 
+        {//но если нам понадобится вызвать менюшку хотя бы в 2х и больше мест, то этот метод оправдан. так что считай, что это на вырост
+            Console.WriteLine("Введите команду \n 1 - запуск двиг., \n 2 - остановка двиг., \n 3 - заправка, \n 4 - увелич. скорость, \n 5 - уменьшить скорость, \n 6 - увелич. высоту, \n 7 - уменьш. высоту.");
+        }
+        static void StartProgramm()       //метод, создающий объекты самолёт и заносящий их в Авиапарк. принимаемый параметр = количеству самолётов в авиапарке
+        { }//хотя этот метод теперь лишний, но вс равно не удаляй. пусть он будет на будущее. ты там ещё создашь нормальный конструктор Самолёту и тогда заюзаешь
+
+        public static void center()//"метод-интерфейс" для пользователя. показывает менюшку и перенаправляет по выбору.
+        {
+            Console.ReadKey();
+            Console.Clear();
+            met.print_menu();//вызов метода-мечати твоей менюшки. там расписанно подробно
+            int command = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();    //чистим консоль... в эстетических целях)
+            switch (command)
+            {
+
+                case 1:
+                    plane.startengine();
+                    break;
+                case 2:
+                    plane.stopengine();
+                    
+                    //if (plane.height > 0)  // тут ты обращался просто к полю метода.. но я даже не понимаю как оно бы сработало у тебя в мэине) либо делай все поля пабликами и тогда не мучайся, либо дела Set и Get для каждого поля класса.
+                    //{
+                    //    Console.WriteLine("Откл. двигателя на высоте > 0. Самолет упал и разбился.");
+                    //}
+                    break;
+                case 3:
+                    plane.getfuel();
+                    break;
+                case 4:
+                    Console.WriteLine("Введите увелич. скорости. (Макс скорость - 700 единиц.)");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    plane.morespeed(i);
+                    break;
+                case 5:
+                    Console.WriteLine("Введите уменьш. скорости. (Макс скорость - 700 единиц.)");
+                    int j = Convert.ToInt32(Console.ReadLine());
+                    plane.lessspeed(j);
+                    break;
+                case 6:
+                    Console.WriteLine("Введите увелич. высоты. (Макс высота - 900 единиц.)");
+                    int t = Convert.ToInt32(Console.ReadLine());
+                    plane.moreheight(t);
+                    break;
+                case 7:
+                    Console.WriteLine("Введите уменьш. высоты. (Макс высота - 900 единиц.)");
+                    int m = Convert.ToInt32(Console.ReadLine());
+                    plane.lessheight(m);
+                    break;
+            }
+
+        }
+        //met.plane.startengine(); а это пример обращения к полю и тд. вот такую штуку вставь в конце каждого метода-действия. 
+    }
+
 
     class Program  //базовый класс
     {
-        //
-        //коллекция Авиапарк объектов класса самолёт
-        //
-        void StartProgramm()       //метод, создающий объекты самолёт и заносящий их в Авиапарк. принимаемый параметр = количеству самолётов в авиапарке
-        {
-            
-            Console.WriteLine("Введите команду (1 - запуск двиг., 2 - остановка двиг., 3 - заправка, 4 - увелич. скорость, 5 - уменьшить скорость, 6 - увелич. высоту, 7 - уменьш. высоту). ");
-            center();
-        }
-        
-        void center()
-        {
-            int command = Convert.ToInt32(Console.ReadLine());
-
-            switch (command)
-            {
-                case 1:
-                    newplane.startengine();
-                    break;
-                case 2:
-                    newplane.stopengine();
-                    if (height > 0)
-                    {
-
-                    }
-                case 3:
-                    newplane.
-
-            }
-            public airport newplane = new airport();
-        }
-
         static void Main(string[] args) //точка входа в программу
         {
-            
-            
-            
-            StartProgramm();      //вызов метода стартпрограмм с передачей количества самолётов
-            
-            
-
-
-            //cl cl1 = new cl(n);         //создание экземпляра класса. тут мы используем конструктор с параметром
-            //cl cl2 = new cl();          //тут конструктор без параметров
-
-
-            //Console.WriteLine(cl1.MetN());      //
-            //Console.WriteLine(cl2.MetN());      //вызов методов 1 у обоих классов
-
-
-            //cl1.MetM(5);        //
-            //cl2.MetM(5);        //вызов второго метода у обоих экземпляров класса 
-
-
-            Console.WriteLine("финиш");
-            Console.ReadKey();
+            met.center();   //просто передача управления в наш метод-интерфейс
         }
     }
 }
+
